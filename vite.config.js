732be9941a175dab429a46d317e4261dbfdd7b19
viacheslavari-sys <viacheslavari@gmail.com>
2026-04-01 17:19:12 +0300

    Bug

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'es2020',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          recharts: ['recharts'],
-          react   : ['react', 'react-dom'],
-        },
-      },
-    },
-  },
 })
